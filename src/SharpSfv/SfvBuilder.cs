ffd389e22ac9953f938097906f93009c71bc4ddc
J. Zebedee <zebedee@code.gripe>
2021-02-22 14:21:57 -0600

    Update IBufferWriter-based SfvBuilder implementation

--- src/SharpSfv/SfvBuilder.cs
+++ src/SharpSfv/SfvBuilder.cs
@@ -8,55 +8,37 @@ namespace SharpSfv
 {
     public sealed class SfvBuilder
     {
-        private IBufferWriter<byte> _writer;
-        private Encoding _encoding;
+        private readonly IBufferWriter<byte> _writer;
+        private readonly Encoding _encoding;
 
-        public SfvBuilder(IBufferWriter<byte> writer, Encoding encoding = null)
+        public SfvBuilder(IBufferWriter<byte> writer, Encoding encoding)
         {
+            if (writer is null)
+                throw new ArgumentNullException(nameof(writer));
+
+            if (encoding is null)
+                throw new ArgumentNullException(nameof(encoding));
+
             _writer = writer;
-            _encoding = encoding ?? Encoding.UTF8;
+            _encoding = encoding;
         }
+        public SfvBuilder(IBufferWriter<byte> writer) : this(writer, Encoding.UTF8) { }
 
         private void WriteLine(string key, uint crc32)
         {
-            //slow path
-            var lineBytes = _encoding.GetBytes($"{key} {crc32:x8}{Environment.NewLine}");
+            var lineBytes = _encoding.GetBytes($"{key} {crc32:X8}{Environment.NewLine}");
             _writer.Write(lineBytes);
-
-            //var keyBytes = _encoding.GetByteCount(key);
-            //Utf8Formatter.TryFormat(crc32, )
-            //_writer.GetSpan(keyBytes + 1 )
         }
 
-        public void AddStream(string key, Stream stream)
+        public void AddStream(string key, Stream stream) => WriteLine(key, ChecksumHelper.FromStream(stream));
+
+        public void AddFile(string path) => WriteLine(Path.GetFileName(path), ChecksumHelper.FromFile(path));
+
+        public void AddDirectory(string path)
         {
-            if (!stream.CanRead)
-                throw new ArgumentException("Stream must be readable", nameof(stream));
-
-            var crc = new Crc32Hash();
-
-            byte[] buffer = ArrayPool<byte>.Shared.Rent(0x1000);
-            try
+            foreach (var file in Directory.EnumerateFiles(path))
             {
-                int bytesRead;
-                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    crc.Append(buffer.AsSpan(0, bytesRead));
-                }
-            }
-            finally
-            {
-                ArrayPool<byte>.Shared.Return(buffer);
-            }
-
-            WriteLine(key, crc.Hash);
-        }
-
-        public void AddFile(string path)
-        {
-            using (var stream = File.OpenRead(path))
-            {
-                AddStream(Path.GetFileName(path), stream);
+                AddFile(file);
             }
         }
     }
