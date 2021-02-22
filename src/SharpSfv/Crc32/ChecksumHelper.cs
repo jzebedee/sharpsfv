3a8ca7e2997088fa834c6668600f7e8f5904535c
J. Zebedee <zebedee@code.gripe>
2021-02-22 14:21:33 -0600

    Add ChecksumHelper

---
+++ src/SharpSfv/Crc32/ChecksumHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+
+namespace SharpSfv.Crc32
+{
+    internal static class ChecksumHelper
+    {
+        public static uint FromStream(Stream stream)
+        {
+            if (!stream.CanRead)
+                throw new ArgumentException("Stream must be readable", nameof(stream));
+
+            var crc = new Crc32Hash();
+
+            var length = stream.Length;
+            int initialSize = length == 0 ? 0x1000 : length > int.MaxValue ? 0x10000 : (int)length;
+
+            byte[] buffer = ArrayPool<byte>.Shared.Rent(initialSize);
+            try
+            {
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    crc.Append(buffer.AsSpan(0, bytesRead));
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+
+            return crc.Hash;
+        }
+
+        public static uint FromFile(string path)
+        {
+            using (var stream = File.OpenRead(path))
+            {
+                return FromStream(stream);
+            }
+        }
+    }
+}
--- src/SharpSfv/Crc32/Crc32Table.cs
+++ src/SharpSfv/Crc32/Crc32Table.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SharpSfv.Crc32
+﻿namespace SharpSfv.Crc32
 {
     internal static class Crc32Table
     {
