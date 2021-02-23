45d4a01e14e54d3ac157ad9df24b4ead0be6f248
J. Zebedee <zebedee@code.gripe>
2021-02-22 19:29:18 -0600

    Use streams for SfvBuilder

--- src/SharpSfv/SfvBuilder.cs
+++ src/SharpSfv/SfvBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using SharpSfv.Crc32;
 using System;
-using System.Buffers;
 using System.IO;
 using System.Text;
 
@@ -8,26 +7,26 @@ namespace SharpSfv
 {
     public sealed class SfvBuilder
     {
-        private readonly IBufferWriter<byte> _writer;
+        private readonly Stream _stream;
         private readonly Encoding _encoding;
 
-        public SfvBuilder(IBufferWriter<byte> writer, Encoding encoding)
+        public SfvBuilder(Stream stream, Encoding encoding)
         {
-            if (writer is null)
-                throw new ArgumentNullException(nameof(writer));
+            if (stream is null)
+                throw new ArgumentNullException(nameof(stream));
 
             if (encoding is null)
                 throw new ArgumentNullException(nameof(encoding));
 
-            _writer = writer;
+            _stream = stream;
             _encoding = encoding;
         }
-        public SfvBuilder(IBufferWriter<byte> writer) : this(writer, Encoding.UTF8) { }
+        public SfvBuilder(Stream stream) : this(stream, Encoding.UTF8) { }
 
         private void WriteLine(string key, uint crc32)
         {
             var lineBytes = _encoding.GetBytes($"{key} {crc32:X8}{Environment.NewLine}");
-            _writer.Write(lineBytes);
+            _stream.Write(lineBytes, 0, lineBytes.Length);
         }
 
         public void AddStream(string key, Stream stream) => WriteLine(key, ChecksumHelper.FromStream(stream));
