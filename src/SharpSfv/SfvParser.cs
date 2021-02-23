4a1ab763c34a13ed1d97104678f2265ac1a2d3e2
J. Zebedee <zebedee@code.gripe>
2021-02-23 00:19:52 -0600

    Add full featured implementation of SfvParser

--- src/SharpSfv/SfvParser.cs
+++ src/SharpSfv/SfvParser.cs
@@ -1,78 +1,72 @@
 ﻿using System;
 using System.Buffers.Text;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SharpSfv
 {
     public sealed class SfvParser
     {
-        //public static SfvParser FromFile(string path)
-        //{
-
-        //}
-        //public static SfvParser FromFile(string path, Encoding encoding)
-        //{
-
-        //}
-        //public static SfvParser FromStream(Stream stream)
-        //{
-
-        //}
-        //public static SfvParser FromStream(Stream stream, Encoding encoding)
-        //{
-
-        //}
-        //public static SfvParser FromLines(IEnumerable<string> lines)
-        //{
-
-        //}
-        //public static SfvParser FromBuffer(ReadOnlySpan<byte> utf8Buffer)
-        //{
-
-        //}
-
-        //private SfvParser()
-        //{
-        //}
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static ReadOnlySpan<byte> ToUtf8(string str) => Encoding.UTF8.GetBytes(str);
-
-        public SfvParser(IEnumerable<string> lines)
+        public static SfvParser FromFile(string path) => FromBuffer(File.ReadAllBytes(path));
+        public static SfvParser FromFile(string path, Encoding encoding) => FromLines(File.ReadLines(path, encoding));
+        public static SfvParser FromStream(Stream stream)
         {
-            foreach (var line in lines)
+            using (var ms = new MemoryStream())
+            using (stream)
             {
-                ParseLine(ToUtf8(line));
+                stream.CopyTo(ms);
+                if (!ms.TryGetBuffer(out var segment))
+                {
+                    throw new InvalidOperationException("failed to buffer stream");
+                }
+
+                return FromBuffer(segment);
             }
         }
-
-        public SfvParser(Stream stream, Encoding encoding = null)
+        public static SfvParser FromStream(Stream stream, Encoding encoding)
         {
+            var parser = new SfvParser();
+
             using (stream)
-            using (var reader = new StreamReader(stream, encoding ?? Encoding.UTF8))
+            using (var reader = new StreamReader(stream, encoding))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    ParseLine(ToUtf8(line));
+                    parser.ParseLine(line);
                 }
             }
+
+            return parser;
         }
-        public SfvParser(string path, Encoding encoding = null)
+        public static SfvParser FromLines(IEnumerable<string> lines)
         {
-            var lines = File.ReadLines(path, encoding ?? Encoding.UTF8);
+            var parser = new SfvParser();
+
             foreach (var line in lines)
             {
-                ParseLine(ToUtf8(line));
+                parser.ParseLine(line);
             }
+
+            return parser;
         }
+        public static SfvParser FromBuffer(ReadOnlySpan<byte> utf8Buffer)
+        {
+            var parser = new SfvParser();
+
+            var newlines = LineEndings.AsSpan();
+            int i;
+            while ((i = utf8Buffer.IndexOfAny(newlines)) != -1)
+            {
+                ReadOnlySpan<byte> line = utf8Buffer.Slice(0, i);
+                parser.ParseLine(line);
+                utf8Buffer = utf8Buffer.Slice(i + 1);
+            }
+
+            return parser;
+        }
+
         public SfvParser()
         {
         }
@@ -80,27 +74,33 @@ namespace SharpSfv
         private const byte CommentPrefix = (byte)';';
         private const byte EntrySeparator = (byte)' ';
 
-        public void ParseLine(ReadOnlySpan<byte> span)
+        private static readonly byte[] LineEndings = Environment.NewLine.ToUtf8();
+
+        private readonly List<SfvLine> _entries = new List<SfvLine>();
+
+        public void ParseLine(string line) => ParseLine(line.ToUtf8());
+
+        public void ParseLine(ReadOnlySpan<byte> utf8line)
         {
-            if (span.IsEmpty)
+            if (utf8line.IsEmpty)
             {
                 return;
             }
 
-            if (span[0] == CommentPrefix)
+            if (utf8line[0] == CommentPrefix)
             {
-                HandleComment(span.Slice(1));
+                HandleComment(utf8line.Slice(1));
                 return;
             }
 
-            var separator = span.IndexOf(EntrySeparator);
+            var separator = utf8line.IndexOf(EntrySeparator);
             if (separator == -1)
             {
                 throw new InvalidOperationException("bad format");
             }
 
-            var key = span.Slice(0, separator);
-            if (!Utf8Parser.TryParse(span.Slice(separator + 1), out uint crc32, out _, standardFormat: 'x'))
+            var key = utf8line.Slice(0, separator);
+            if (!Utf8Parser.TryParse(utf8line.Slice(separator + 1), out uint crc32, out _, standardFormat: 'x'))
             {
                 throw new InvalidOperationException("bad format");
             }
@@ -110,14 +110,24 @@ namespace SharpSfv
 
         private void HandleComment(ReadOnlySpan<byte> span)
         {
-            throw new NotImplementedException();
-            //Debug.WriteLine($"comment:{Encoding.UTF8.GetString(span.ToArray())}");
+            string commentString;
+#if NET
+            commentString = Encoding.UTF8.GetString(span);
+#else //slow path with copy
+            commentString = Encoding.UTF8.GetString(span.ToArray());
+#endif
+            _entries.Add(new SfvLine(SfvLineType.Comment, commentString, default));
         }
 
-        private void HandleEntry(ReadOnlySpan<byte> key, uint crc32)
+        private void HandleEntry(ReadOnlySpan<byte> span, uint crc32)
         {
-            throw new NotImplementedException();
-            //Debug.WriteLine($"key:{Encoding.UTF8.GetString(key.ToArray())} crc32:{crc32:X8}");
+            string keyString;
+#if NET
+            keyString = Encoding.UTF8.GetString(span);
+#else //slow path with copy
+            keyString = Encoding.UTF8.GetString(span.ToArray());
+#endif
+            _entries.Add(new SfvLine(SfvLineType.Key, keyString, crc32));
         }
     }
 }
