37dbac9062ba33fede89a3b32c3a45e5bcc6b0cb
J. Zebedee <zebedee@code.gripe>
2021-02-22 23:56:42 -0600

    Add SfvLine

---
+++ src/SharpSfv/SfvLine.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpSfv
+{
+    public readonly struct SfvLine
+    {
+        public readonly SfvLineType Type;
+
+        public readonly string Entry;
+
+        public readonly uint Crc32;
+
+        public SfvLine(SfvLineType type, string entry, uint crc32)
+        {
+            Type = type;
+            Entry = entry;
+            Crc32 = crc32;
+        }
+    }
+}
