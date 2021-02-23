b8b1e718a3bd4e820e8d05db215f7ae3009279f1
J. Zebedee <zebedee@code.gripe>
2021-02-22 19:29:46 -0600

    Implement SfvBuilder tests

--- test/SharpSfv.Tests/SfvBuilderTests.cs
+++ test/SharpSfv.Tests/SfvBuilderTests.cs
@@ -1,7 +1,8 @@
+using SharpSfv.Crc32;
 using System;
-using System.Buffers;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -17,22 +18,111 @@ namespace SharpSfv.Tests
             return input;
         }
 
-        [Fact]
-        public void BuildInMemorySfvTest()
+        public static IEnumerable<object[]> Encodings
         {
-            throw new NotImplementedException();
+            get
+            {
+                //should default to UTF8
+                yield return new object[] { null };
+                yield return new object[] { Encoding.ASCII };
+
+            }
         }
 
-        [Fact]
-        public void BuildFileSfvTest()
+        private static SfvBuilder GetBuilder(Stream outputStream, Encoding encoding)
         {
-            throw new NotImplementedException();
+            SfvBuilder builder;
+            if (encoding is null)
+            {
+                builder = new SfvBuilder(outputStream);
+            }
+            else
+            {
+                builder = new SfvBuilder(outputStream, encoding);
+            }
+            return builder;
         }
 
-        [Fact]
-        public void BuildDirectorySfvTest()
+        [Theory]
+        [MemberData(nameof(Encodings))]
+        public void BuildInMemorySfvTest(Encoding encoding)
         {
-            throw new NotImplementedException();
+            const string key = "Þórsteinn";
+            var outputStream = new MemoryStream();
+
+            var builder = GetBuilder(outputStream, encoding);
+            encoding = encoding ?? Encoding.UTF8;
+
+            var randomBuffer = GetRandomBuffer(0x1000);
+            builder.AddStream(key, new MemoryStream(randomBuffer));
+
+            var expected = encoding.GetBytes($"{key} {(new Crc32Hash().Compute(randomBuffer)):X8}{Environment.NewLine}");
+            var actual = outputStream.ToArray();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(Encodings))]
+        public void BuildFileSfvTest(Encoding encoding)
+        {
+            const string key = "Þórsteinn";
+            string filepath = Path.ChangeExtension(Path.GetTempFileName(), $"{key}.tmp");
+            string filename = Path.GetFileName(filepath);
+
+            var randomBuffer = GetRandomBuffer(0x1000);
+            File.WriteAllBytes(filepath, randomBuffer);
+
+            string sfvFilepath = Path.ChangeExtension(Path.GetTempFileName(), ".sfv.tmp");
+            using (var outputStream = File.Create(sfvFilepath))
+            {
+                var builder = GetBuilder(outputStream, encoding);
+                builder.AddFile(filepath);
+            }
+            encoding = encoding ?? Encoding.UTF8;
+
+            var expected = encoding.GetBytes($"{filename} {(new Crc32Hash().Compute(randomBuffer)):X8}{Environment.NewLine}");
+            var actual = File.ReadAllBytes(sfvFilepath);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(Encodings))]
+        public void BuildDirectorySfvTest(Encoding encoding)
+        {
+            const string key = "Þórsteinn";
+
+            string dir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(dir);
+
+            var testFiles = Enumerable.Range(0, 10).Select(i => new
+            {
+                filename = key + i,
+                content = GetRandomBuffer(0x1000)
+            }).Select(a => new
+            {
+                path = Path.Combine(dir, a.filename),
+                a.content,
+                expected = $"{a.filename} {(new Crc32Hash().Compute(a.content)):X8}{Environment.NewLine}"
+            });
+
+            string expectedStr = "";
+            foreach (var a in testFiles)
+            {
+                expectedStr += a.expected;
+                File.WriteAllBytes(a.path, a.content);
+            }
+
+            string sfvFilepath = Path.ChangeExtension(Path.GetTempFileName(), ".sfv.tmp");
+            using (var outputStream = File.Create(sfvFilepath))
+            {
+                var builder = GetBuilder(outputStream, encoding);
+                builder.AddDirectory(dir);
+            }
+            encoding = encoding ?? Encoding.UTF8;
+
+            var expected = encoding.GetBytes(expectedStr);
+            var actual = File.ReadAllBytes(sfvFilepath);
+            Assert.Equal(expected, actual);
         }
     }
 }
