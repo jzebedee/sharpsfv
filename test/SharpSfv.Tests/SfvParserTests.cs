1df61dcb204315d0eb23cb0e61534eaf8280379e
J. Zebedee <zebedee@code.gripe>
2021-02-23 00:21:56 -0600

    Break out SfvParserTests into separate facts

--- test/SharpSfv.Tests/SfvParserTests.cs
+++ test/SharpSfv.Tests/SfvParserTests.cs
@@ -1,36 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Xunit;
 
 namespace SharpSfv.Tests
 {
     public class SfvParserTests
     {
-        private const string GoodSfv0 = "test.txt DEADBEEF";
+        //private static readonly char[] LineEndings = Environment.NewLine.ToCharArray();
+        //
+        //[Theory]
+        //[InlineData(GoodSfv0, true)]
+        //[InlineData(GoodSfv1, true)]
+        //public void ParseSfvDoesntThrowTest(string sfv, bool good)
+        //{
+        //    var parser = new SfvParser();
+        //    foreach(var line in sfv.Split(LineEndings))
+        //    {
+        //        ReadOnlySpan<byte> span = Encoding.UTF8.GetBytes(line);
+        //        parser.ParseLine(span);
+        //    }
+        //}
 
-        private const string GoodSfv1 =
-            "dummy.txt DEADBEEF\r\n" +
-            "dummy1.txt BEEFDEED\r\n" +
-            "; just taking a break\r\n" +
-            "x DADADADA";
+        private const string GoodSingleLineSfv = "test.txt DEADBEEF";
 
-        private static readonly char[] LineEndings = Environment.NewLine.ToCharArray();
-
-        [Theory]
-        [InlineData(GoodSfv0, true)]
-        [InlineData(GoodSfv1, true)]
-        public void ParseSfvTest(string sfv, bool good)
+        [Fact]
+        public void ParseSingleLineSfvTest()
         {
-            var parser = new SfvParser();
-            foreach(var line in sfv.Split(LineEndings))
-            {
-                ReadOnlySpan<byte> span = Encoding.UTF8.GetBytes(line);
-                parser.ParseLine(span);
-            }
+            var parser = SfvParser.FromLines(new string[] { GoodSingleLineSfv });
+            var entries = parser.Entries;
+            Assert.NotNull(entries);
+            Assert.NotEmpty(entries);
+
+            var expected = new SfvLine(SfvLineType.Key, "test.txt", 0xDEADBEEF);
+            Assert.Equal(expected, entries[0]);
+        }
+
+        private const string GoodMultiLineSfv =
+           "dummy.txt DEADBEEF\r\n" +
+           "dummy1.txt BEEFDEED\r\n" +
+           "; just taking a break\r\n" +
+           "x DADADADA";
+
+        [Fact]
+        public void ParseMultiLineSfvTest()
+        {
+            var parser = SfvParser.FromBuffer(Encoding.UTF8.GetBytes(GoodMultiLineSfv));
+            var entries = parser.Entries;
+            Assert.NotNull(entries);
+            Assert.Equal(4, entries.Count);
+
+            var expected1 = new SfvLine(SfvLineType.Key, "dummy.txt", 0xDEADBEEF);
+            Assert.Equal(expected1, entries[0]);
+ 
+            var expected2 = new SfvLine(SfvLineType.Key, "dummy1.txt", 0xBEEFDEED);
+            Assert.Equal(expected2, entries[1]);
+
+            var expected3 = new SfvLine(SfvLineType.Comment, " just taking a break", default);
+            Assert.Equal(expected3, entries[2]);
+
+            var expected4 = new SfvLine(SfvLineType.Key, "x", 0xDADADADA);
+            Assert.Equal(expected4, entries[3]);
         }
     }
 }
